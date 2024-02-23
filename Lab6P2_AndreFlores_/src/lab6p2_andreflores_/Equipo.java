d55ea5d80b35b34bd35c816d5e7904642b34f837
Andre-Flores <andrefloresromero@gmail.com>
2024-02-23 14:33:05 -0600

    jdialog crear jugadores y clase jugador hecha

--- Lab6P2_AndreFlores_/src/lab6p2_andreflores_/Equipo.java
+++ Lab6P2_AndreFlores_/src/lab6p2_andreflores_/Equipo.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lab6p2_andreflores_;
 
-/**
- *
- * @author andre
- */
 public class Equipo {
+    private String paisEquipo;
+    private String nombreEquipo;
+    private String ciudad;
+    private String Estadio;
+
+    public Equipo(String paisEquipo, String nombreEquipo, String ciudad, String Estadio) {
+        this.paisEquipo = paisEquipo;
+        this.nombreEquipo = nombreEquipo;
+        this.ciudad = ciudad;
+        this.Estadio = Estadio;
+    }
+
+    public String getPaisEquipo() {
+        return paisEquipo;
+    }
+
+    public void setPaisEquipo(String paisEquipo) {
+        this.paisEquipo = paisEquipo;
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstadio() {
+        return Estadio;
+    }
+
+    public void setEstadio(String Estadio) {
+        this.Estadio = Estadio;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo: " + "pais del equipo: " + paisEquipo + ", nombre del equipo: " + nombreEquipo + ", ciudad: " + ciudad + ", Estadio: " + Estadio;
+    }
     
 }
